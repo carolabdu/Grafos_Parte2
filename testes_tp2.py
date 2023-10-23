830b5f8ed910dc571c550d67a76040b11e75bb1d
carolabdu <108949383+carolabdu@users.noreply.github.com>
2023-10-23 18:53:30 -0300

    Update and rename saida_tp2.py to testes_tp2.py

---
+++ testes_tp2.py
@@ -0,0 +1,43 @@
+from TP2 import *
+
+with open('grafo_W_4.txt', 'r') as arquivo:  #mudar o número do grafo de acordo com o teste 
+        texto = [vertice for vertice in arquivo.read().split()  ] #Cria uma lista com o conteúdo presente no arquivo de teste
+        vertices = int(texto[0]) #Pega o primeiro elemento da lista acima, que correnspode ao número de vértices do grafo
+        arestas = [] #Lista vazia que vai conter todos os pares de arestas do grafo
+        for i in range(len(texto[1:])//3):
+            arestas += [[int(texto[3*i+1])]+[int(texto[3*i+2])]+ [float(texto[3*i+3])]] 
+
+
+grafo = Graph_l(vertices, arestas)
+
+#Questão 1: (ir mudando v2 entre 20, 30, 40, 50 e 60)
+v_teste = [20,30,40,50,60]
+for vf in v_teste: 
+    grafo.distancia(10,vf,1)  
+    grafo.caminho(10,vf,1)       
+
+#Questão 2: 
+
+import time
+import random
+
+tempo_djk = 0
+tempo_heap = 0 
+
+for i in range(20):
+    vertice = random.randint(1,grafo.v)
+    
+    inicio_djk = time.time()
+    grafo.DijkstraHeap(vertice,0)
+    fim_djk = time.time()
+
+    inicio_heap = time.time()
+    grafo.Dijkstra(vertice,0)
+    fim_heap = time.time()
+    
+    tempo_djk += fim_djk - inicio_djk
+    tempo_heap += fim_heap - inicio_heap
+
+print ('Tempo sem heap:', tempo_djk/20, 'Tempo com heap:', tempo_heap/20)
+
+#Questão 3: 
