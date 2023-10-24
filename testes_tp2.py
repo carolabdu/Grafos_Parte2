f941cd3cb8152e7951368aad659683403b2b20de
Alice Duarte F Ribeiro <108374759+alicedfr@users.noreply.github.com>
2023-10-24 02:39:32 -0300

    Update testes_tp2.py

--- testes_tp2.py
+++ testes_tp2.py
@@ -40,4 +40,29 @@ for i in range(20):
 
 print ('Tempo sem heap:', tempo_djk/20, 'Tempo com heap:', tempo_heap/20)
 
-#Questão 3: 
+#Questão 3 (Rede de colaboração):
+
+with open('rede_colaboracao_vertices.txt', 'r', encoding='utf-8') as arquivo:
+    colaboradores = [x.rstrip('\n').split(',') for x in arquivo.readlines()]
+    dic = {}
+    for i in range(len(colaboradores)):
+        dic[colaboradores[i][1]] = int(i+1)
+            
+with open('rede_colaboracao.txt', 'r', encoding='utf-8') as arquivo:
+        texto = [vertice for vertice in arquivo.read().split()  ] #Cria uma lista com o conteúdo presente no arquivo de teste
+        vertices = int(texto[0]) #Pega o primeiro elemento da lista acima, que correnspode ao número de vértices do grafo
+        arestas = [] #Lista vazia que vai conter todos os pares de arestas do grafo
+        for i in range(len(texto[1:])//3):
+            arestas += [[int(texto[3*i+1])]+[int(texto[3*i+2])]+ [float(texto[3*i+3])]] #Atualiza a lista de arestas, criando sublistas com os pares de arestas
+
+
+G = Graph_l(vertices, arestas)
+origem = 'Edsger W. Dijkstra'
+pesquisadores = ['Alan M. Turing', 'J. B. Kruskal', 'Jon M. Kleinberg', 'Daniel R. Figueiredo', 'Éva Tardos']
+
+
+for i in range(len(pesquisadores)):
+    d = G.distancia(dic[origem], dic[pesquisadores[i]], 0)
+    caminho_min = G.caminho(dic[origem], dic[pesquisadores[i]], 0)
+    print(f'Distância entre {origem} e {pesquisadores[i]}: {d}')
+    print(f'Caminho minimo entre {origem} e {pesquisadores[i]}: {caminho_min}')
